1cf44addc0fa7278635298f542d5a42f1ac505e0
TechQuery <shiy007@qq.com>
2019-10-19 20:13:19 +0800

    [add] Test Framework based on Jest [refactor] Card() supports Horizontal layout

--- source/Card.tsx
+++ source/Card.tsx
@@ -1,43 +1,63 @@
 import { createCell } from 'web-cell';
+import classNames from 'classnames';
 
-interface CardLink {
-    path: string;
-    text?: string;
-    level?: string;
+interface CardWrapperProps {
+    direction?: 'horizontal' | 'vertical';
+    left: any;
+    right: any;
 }
-interface CardData {
+
+function CardWrapper({
+    direction = 'vertical',
+    left,
+    right
+}: CardWrapperProps) {
+    return direction === 'vertical' ? (
+        [].concat(left, right)
+    ) : (
+        <div class="row no-gutters align-items-center">
+            <div class="col-sm-4">{left}</div>
+            <div class="col-sm-8">{right}</div>
+        </div>
+    );
+}
+
+interface CardProps {
     className?: string;
     title: string;
-    link: CardLink;
+    text?: string;
     image?: any;
+    direction?: CardWrapperProps['direction'];
     children?: any;
 }
 
 export function Card({
     className = '',
     title,
-    link,
+    text,
     image,
+    direction,
     children
-}: CardData) {
+}: CardProps) {
     return (
-        <div className={`card ${className}`}>
-            {typeof image !== 'string' ? (
-                image
-            ) : (
-                <img className="card-img-top" src={image} />
-            )}
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{children}</p>
-                <a
-                    className={`btn btn-${link.level || 'primary'}`}
-                    href={link.path}
-                    title={title}
-                >
-                    {link.text || 'Enter'}
-                </a>
-            </div>
+        <div className={classNames('card', className)}>
+            <CardWrapper
+                direction={direction}
+                left={
+                    typeof image !== 'string' ? (
+                        image
+                    ) : (
+                        <img className="card-img-top" src={image} />
+                    )
+                }
+                right={
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        {text && <p className="card-text">{text}</p>}
+                        {children}
+                    </div>
+                }
+            />
         </div>
     );
 }
---
+++ test/Card.spec.tsx
@@ -0,0 +1,37 @@
+import './DOM-polyfill';
+import { createCell } from 'web-cell';
+import { assertLooksLike } from 'snabbdom-looks-like';
+
+import { Card } from '../source/Card';
+
+describe('Card', () => {
+    it('should render Vertical layout by default', () => {
+        assertLooksLike(
+            <Card title="Test" image="test.png" />,
+            <div className="card">
+                <img className="card-img-top" src="test.png" />
+                <div className="card-body">
+                    <h5 className="card-title">Test</h5>
+                </div>
+            </div>
+        );
+    });
+
+    it('should render Horizontal layout as "direction" set', () => {
+        assertLooksLike(
+            <Card title="Test" image="test.png" direction="horizontal" />,
+            <div className="card">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-sm-4">
+                        <img className="card-img-top" src="test.png" />
+                    </div>
+                    <div class="col-sm-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Test</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    });
+});
