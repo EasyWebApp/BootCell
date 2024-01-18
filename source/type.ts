2dc20138621ae2b7def9bab80762ccfc3ba67750
South Drifted <shiy2008@gmail.com>
2024-01-18 22:43:10 +0000

    [refactor] rewrite Badge, Spinner, Jumbotron & Card components

--- source/type.ts
+++ source/type.ts
@@ -1,18 +1,20 @@
 type Subtle<T extends string> = `${T}${'' | '-subtle'}`;
 
-export type Color =
-    | 'primary'
-    | 'secondary'
-    | 'success'
-    | 'danger'
-    | 'warning'
-    | 'info'
-    | 'light'
-    | 'dark';
+export enum Status {
+    primary = 'primary',
+    secondary = 'secondary',
+    tertiary = 'tertiary',
+    success = 'success',
+    info = 'info',
+    warning = 'warning',
+    danger = 'danger'
+}
+
+export type Color = Exclude<keyof typeof Status, 'tertiary'> | 'light' | 'dark';
 
 export type BackgroundColor =
     | Subtle<Color>
-    | `body${'' | '-secondary' | '-tertiary'}`
+    | `body${'' | '-emphasis' | '-secondary' | '-tertiary'}`
     | 'black'
     | 'white'
     | 'transparent';
