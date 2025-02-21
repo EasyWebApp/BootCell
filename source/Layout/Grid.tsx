e9c5620ceaf5c03f9436c42ccba910cf683f46c5
TechQuery <shiy2008@gmail.com>
2025-02-21 15:17:57 +0800

    [add] Row & Col components [refactor] rewrite Alert & BreadCrumb components to WebCell v3 [optimize] Class updating of Nav component [fix] Pagination margin bugs [optimize] update Upstream packages

--- source/Layout/Grid.tsx
+++ source/Layout/Grid.tsx
@@ -1,9 +1,9 @@
-import { JsxProps } from 'dom-renderer';
-import { FC } from 'web-cell';
+import classNames from 'classnames';
+import { FC, WebCellProps } from 'web-cell';
 
 import { Size } from '../type';
 
-export interface ContainerProps extends JsxProps<HTMLDivElement> {
+export interface ContainerProps extends WebCellProps {
     fluid?: boolean | Size;
 }
 
@@ -22,3 +22,77 @@ export const Container: FC<ContainerProps> = ({
         {children}
     </div>
 );
+
+export type RowProps = Record<'xs' | Size, number | 'auto'> & WebCellProps;
+
+export const Row: FC<RowProps> = ({
+    className,
+    xs,
+    sm,
+    md,
+    lg,
+    xl,
+    xxl,
+    children,
+    ...props
+}) => {
+    const size = xs || sm || md || lg || xl || xxl;
+    const columnClass = size
+        ? `row-cols${
+              xs
+                  ? ''
+                  : sm
+                    ? '-sm'
+                    : md
+                      ? '-md'
+                      : lg
+                        ? '-lg'
+                        : xl
+                          ? '-xl'
+                          : xxl && '-xxl'
+          }-${size}`
+        : '';
+
+    return (
+        <div className={classNames('row', columnClass, className)} {...props}>
+            {children}
+        </div>
+    );
+};
+
+export type ColProps = RowProps;
+
+export const Col: FC<ColProps> = ({
+    className,
+    xs,
+    sm,
+    md,
+    lg,
+    xl,
+    xxl,
+    children,
+    ...props
+}) => {
+    const size = xs || sm || md || lg || xl || xxl;
+    const columnClass = size
+        ? `col${
+              xs
+                  ? ''
+                  : sm
+                    ? '-sm'
+                    : md
+                      ? '-md'
+                      : lg
+                        ? '-lg'
+                        : xl
+                          ? '-xl'
+                          : xxl && '-xxl'
+          }-${size}`
+        : '';
+
+    return (
+        <div className={classNames('col', columnClass, className)} {...props}>
+            {children}
+        </div>
+    );
+};
